7b78a90de0370fac0ead43d993d2154696feb757
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2025-02-16 17:39:28 +0700

    Коммит с проверкой, что все работает

--- src/test/java/ru/tpu/hostel/user/UserApplicationTests.java
+++ src/test/java/ru/tpu/hostel/user/UserApplicationTests.java
@@ -3,11 +3,14 @@ package ru.tpu.hostel.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 //@SpringBootTest
 class UserApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
