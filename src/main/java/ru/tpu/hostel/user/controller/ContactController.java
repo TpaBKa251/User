935f7be9b155aa1194e8828da27dad9f8442590d
gazco <gazcoin-1@bk.ru>
2026-02-11 22:25:45 +0700

    Фикс DELETE метода

--- src/main/java/ru/tpu/hostel/user/controller/ContactController.java
+++ src/main/java/ru/tpu/hostel/user/controller/ContactController.java
@@ -1,6 +1,7 @@
 package ru.tpu.hostel.user.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -11,10 +12,10 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -82,9 +83,11 @@ public class ContactController {
         return contactService.getAllUsersWithRoleContacts();
     }
 
-    @DeleteMapping()
-    public void deleteContact(@RequestParam UUID id) {
-        contactService.deleteContact(id);
+    @DeleteMapping("/{contactId}")
+    public void deleteContact(
+            @Parameter(description = "ID кастомного контакта") @PathVariable UUID contactId
+    ) {
+        contactService.deleteContact(contactId);
     }
 
     @Operation(
